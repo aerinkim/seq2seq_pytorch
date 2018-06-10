22f9e47a9f9cdd98e611675dd73c0b044753a929
Aerin Kim <ahkim@microsoft.com>
2018-06-10 00:38:24 -0700

    encoder & decoder are added

--- main.py
+++ main.py
@@ -140,9 +140,67 @@ input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
 print(random.choice(pairs))
 
 
+#With a seq2seq model the encoder creates a single vector which, 
+#in the ideal case, encodes the “meaning” of the input sequence into 
+#a SINGLE vector — a single point in some N dimensional space of sentences.
 
 
+###################
+# Encoder
+###################
 
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+###################
+# Decoder
+###################
+
+"""
+In the simplest seq2seq decoder we use only last output of the encoder. 
+This last output is sometimes called the context vector 
+as it encodes context from the entire sequence. 
+This context vector is used as the initial hidden state of the decoder.
+
+At every step of decoding, the decoder is given an input token and hidden state.
+The initial input token is the start-of-string <SOS> token, 
+and the first hidden state is the context vector (the encoder’s last hidden state).
+"""
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
 
 
 
