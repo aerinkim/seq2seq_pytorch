1bd133b2953e1ef4173090c71ac4ea102d944cf8
Aerin Kim <aeirnykim@gmail.com>
2018-06-10 17:37:49 -0700

    made 0.4 compatible

---
+++ DecoderRNN.py
@@ -0,0 +1,43 @@
+"""
+In the simplest seq2seq decoder we use only last output of the encoder. 
+This last output is sometimes called the context vector 
+as it encodes context from the entire sequence. 
+This context vector is used as the initial hidden state of the decoder.
+
+At every step of decoding, the decoder is given an input token and hidden state.
+The initial input token is the start-of-string <SOS> token, 
+and the first hidden state is the context vector (the encoder’s last hidden state).
+"""
+
+from __future__ import unicode_literals, print_function, division
+from io import open
+import unicodedata
+import string
+import re
+import random
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
