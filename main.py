05fa3fd033c70ce8214c8c209fe96233cba32be9
Aerin Kim <aerinykim@gmail.com>
2018-06-11 00:45:09 -0700

    added training code

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 
-#device = torch.device({"cuda"} if torch.cuda.is_available() else {"cpu"})
+from DecoderRNN import DecoderRNN
+from EncoderRNN import EncoderRNN
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 SOS_token = 0
@@ -141,72 +145,170 @@ print(random.choice(pairs))
 
 
 #With a seq2seq model the encoder creates a single vector which, 
-#in the ideal case, encodes the “meaning” of the input sequence into 
-#a SINGLE vector — a single point in some N dimensional space of sentences.
+#in the ideal case, encodes the "meaning" of the input sequence into 
+#a SINGLE vector - a single point in some N dimensional space of sentences.
 
 
-###################
-# Encoder
-###################
 
-class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(EncoderRNN, self).__init__()
-        self.hidden_size = hidden_size
+# Preparing training data 
 
-        self.embedding = nn.Embedding(input_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
+#To train, for each pair we will need an input tensor 
+#(indexes of the words in the input sentence) 
+#and target tensor (indexes of the words in the target sentence). 
+#While creating these vectors we will append the EOS token to both sequences.
 
-    def forward(self, input, hidden):
-        embedded = self.embedding(input).view(1, 1, -1)
-        output = embedded
-        output, hidden = self.gru(output, hidden)
-        return output, hidden
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
 
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
 
-###################
-# Decoder
-###################
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
 
+
+def tensorsFromPair(pair):
+    input_tensor = tensorFromSentence(input_lang, pair[0])
+    target_tensor = tensorFromSentence(output_lang, pair[1])
+    return (input_tensor, target_tensor)
+
+
+#Training the Model
 """
-In the simplest seq2seq decoder we use only last output of the encoder. 
-This last output is sometimes called the context vector 
-as it encodes context from the entire sequence. 
-This context vector is used as the initial hidden state of the decoder.
+To train we run the input sentence through the encoder,
+and keep track of every output and the latest hidden state.
+Then the decoder is given the <SOS> token as its first input,
+and the last hidden state of the encoder as its first hidden state.
 
-At every step of decoding, the decoder is given an input token and hidden state.
-The initial input token is the start-of-string <SOS> token, 
-and the first hidden state is the context vector (the encoder’s last hidden state).
+"Teacher forcing" is the concept of using the real target outputs 
+as each next input, instead of using the decoder’s guess as the next input. 
+Using teacher forcing causes it to converge faster 
+but when the trained network is exploited, it may exhibit instability.
+
+You can observe outputs of teacher-forced networks that
+read with coherent grammar but wander far from the correct translation
+- intuitively it has learned to represent the output grammar and can "pick up"
+the meaning once the teacher tells it the first few words, 
+but it has not properly learned how to create the sentence
+from the translation in the first place.
+
+Because of the freedom PyTorch’s autograd gives us, 
+we can randomly choose to use teacher forcing or not with a simple if statement. 
+Turn teacher_forcing_ratio up to use more of it.
 """
 
-
-class DecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size):
-        super(DecoderRNN, self).__init__()
-        self.hidden_size = hidden_size
-
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, input, hidden):
-        output = self.embedding(input).view(1, 1, -1)
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
-        output = self.softmax(self.out(output[0]))
-        return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
+teacher_forcing_ratio = 0.5
 
 
+def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=MAX_LENGTH):
+    encoder_hidden = encoder.initHidden()
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    input_length = input_tensor.size(0)
+    target_length = target_tensor.size(0)
+
+    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+    loss = 0
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(
+            input_tensor[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0, 0]
+
+    decoder_input = torch.tensor([[SOS_token]], device=device)
+
+    decoder_hidden = encoder_hidden
+
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    if use_teacher_forcing:
+        # Teacher forcing: Feed the target as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            loss += criterion(decoder_output, target_tensor[di])
+            decoder_input = target_tensor[di]  # Teacher forcing
+
+    else:
+        # Without teacher forcing: use its own predictions as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            topv, topi = decoder_output.topk(1)
+            decoder_input = topi.squeeze().detach()  # detach from history as input
+
+            loss += criterion(decoder_output, target_tensor[di])
+            if decoder_input.item() == EOS_token:
+                break
+
+    loss.backward()
+
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item() / target_length
+
+# helper functions
+
+import time
+import math
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
 
 
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
 
 
+# Training begins!
+
+#Start a timer
+#Initialize optimizers and criterion
+#Create set of training pairs
+#Start empty losses array for plotting    
 
 
+def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, learning_rate=0.01):
+    start = time.time()
+    plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
 
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    training_pairs = [tensorsFromPair(random.choice(pairs))
+                      for i in range(n_iters)]
+    criterion = nn.NLLLoss()
+
+    for iter in range(1, n_iters + 1):
+        training_pair = training_pairs[iter - 1]
+        input_tensor = training_pair[0]
+        target_tensor = training_pair[1]
+
+        loss = train(input_tensor, target_tensor, encoder,
+                     decoder, encoder_optimizer, decoder_optimizer, criterion)
+        print_loss_total += loss
+        plot_loss_total += loss
+
+        if iter % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),
+                                         iter, iter / n_iters * 100, print_loss_avg))
+
+        if iter % plot_every == 0:
+            plot_loss_avg = plot_loss_total / plot_every
+            plot_losses.append(plot_loss_avg)
+            plot_loss_total = 0
+
+    showPlot(plot_losses)
