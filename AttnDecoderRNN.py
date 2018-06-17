bcabb857ab0fbfbd5794b322e10489d0d8ede163
Aerin Kim <ohmyheroine@hotmail.com>
2018-06-16 23:29:16 -0700

    dimension added on Attention Decoder

--- AttnDecoderRNN.py
+++ AttnDecoderRNN.py
@@ -21,27 +21,37 @@ class AttnDecoderRNN(nn.Module):
         self.max_length = max_length
 
         self.embedding = nn.Embedding(self.output_size, self.hidden_size)
-        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length) 
+        #            Linear : ( input column dim, output column dim )
+        #            output will be ( input row dim, Linear[1] )
         self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
         self.dropout = nn.Dropout(self.dropout_p)
         self.gru = nn.GRU(self.hidden_size, self.hidden_size)
         self.out = nn.Linear(self.hidden_size, self.output_size)
 
-    def forward(self, input, hidden, encoder_outputs):
+    def forward(self, input, hidden, encoder_outputs): 
+        # dimesions : [1,256], [1,256], [10,256]
+        # input is a decoder output. # encoder_outputs is stacked h's.
         embedded = self.embedding(input).view(1, 1, -1)
         embedded = self.dropout(embedded)
 
         attn_weights = F.softmax(
             self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
-        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
-                                 encoder_outputs.unsqueeze(0))
-
-        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        # [1,10] = [ 1, 256*2 ] * [256*2, 10]
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),   # torch.Size([1, 1, 10])
+                                 encoder_outputs.unsqueeze(0))# torch.Size([1, 10, 256])
+        # torch.Size([1, 1, 256]) =  torch.Size([1, 1, 10]) * torch.Size([1, 10, 256])
+        
+        output = torch.cat((embedded[0], attn_applied[0]), 1) 
+        #                    [1,256]         [1,256]
+        # Why concatenate? This is how we code for
+        # P(y_i|y1,y2,y3...) = g(y_i-1, S_i-1, C_i)
         output = self.attn_combine(output).unsqueeze(0)
-
+        # Then we use linear combination to make this combind 'S' and 'C' dimension same as that of hidden. (so that we can put it in GRU.)
+        #[1,1,256]       
         output = F.relu(output)
         output, hidden = self.gru(output, hidden)
-
+        #                         [1,256],[1,256]                            
         output = F.log_softmax(self.out(output[0]), dim=1)
         return output, hidden, attn_weights
 
